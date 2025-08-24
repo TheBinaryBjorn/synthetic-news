f8f2cdabca2231c2daa72376e2c0901930da9212
DanielFeldman1 <daniel.feldman303@gmail.com>
2025-08-24 12:28:03 +0300

    refactor(llm_service): make code pylint compliant
    Rename LLM_Service class to LlmService to follow PascalCase. Add doc strings.

--- backend/services/llm_service.py
+++ backend/services/llm_service.py
@@ -1,20 +1,35 @@
+"""
+This module is in charge of sending messages to an LLM model.
+"""
 import os
 from abc import ABC, abstractmethod
 from dotenv import load_dotenv
 from google import genai
 
-class LLM_Service(ABC):
+class LlmService(ABC):
+    """
+    Abstract class for all LLM Services.
+    """
     @abstractmethod
     def send_message_to_llm(self,text: str) -> str:
         pass
 
-class GeminiService(LLM_Service):
+class GeminiService(LlmService):
+    """
+    An LlmService that utilizes Gemini API for LLM communication.
+    """
     def __init__(self):
+        """
+        Initializes the Service with the Gemini API key.
+        """
         load_dotenv()
-        GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-        self.client = genai.Client(api_key=GEMINI_API_KEY)
+        gemini_api_key = os.getenv("GEMINI_API_KEY")
+        self.client = genai.Client(api_key=gemini_api_key)
         self.model = 'gemini-2.5-flash'
 
     def send_message_to_llm(self,text):
+        """
+        Sends a given text to the LLM model.
+        """
         response = self.client.models.generate_content(model=self.model,contents=text)
         return response.text
