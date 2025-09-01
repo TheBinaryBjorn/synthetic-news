dedbd0e1a975d3157de5b86cbda85ed2f59f28a9
DanielFeldman1 <daniel.feldman303@gmail.com>
2025-09-01 11:41:39 +0300

    test(tts_service): add more tests and type and value checks for tts_service.py
    Added test cases 005 - 012, reaching 97% code coverage in tts_service, also added type and value checks for all parameters in GoogleTtsService methods (input checks).

--- backend/services/tts_service.py
+++ backend/services/tts_service.py
@@ -36,7 +36,7 @@ class GoogleTtsService(TtsService):
 
     def convert_text_to_speech(self, text: str) -> gTTS:
         """
-        Receives a text input and creates an mp3 speech file.
+        Receives a text input and creates an gTTS object.
 
         Args:
             text (str): The text to convert to speech.
@@ -46,6 +46,8 @@ class GoogleTtsService(TtsService):
         """
         if not isinstance(text, str):
             raise TypeError
+        if text == "":
+            raise ValueError
         tts = gTTS(text=text, lang="en", slow=False)
         return tts
 
@@ -60,6 +62,10 @@ class GoogleTtsService(TtsService):
         Returns:
             str: The file path of the saved MP3 file.
         """
+        if not isinstance(topic, str) or not isinstance(text, str):
+            raise TypeError
+        if topic == "" or text == "":
+            raise ValueError
         os.makedirs(self.output_dir, exist_ok=True)
         now = datetime.now()
         timestamp = now.strftime("%U-%Y")
