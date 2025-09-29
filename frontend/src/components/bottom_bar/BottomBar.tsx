61a28ff1f4c1c6cda3db7c696f1547301f53282a
DanielFeldman1 <daniel.feldman303@gmail.com>
2025-09-29 12:04:41 +0300

    style(ui): centered components on bottom bar

--- frontend/src/components/bottom_bar/BottomBar.tsx
+++ frontend/src/components/bottom_bar/BottomBar.tsx
@@ -5,9 +5,15 @@ import VolumeControl from "./VolumeControl";
 export default function BottomBar() {
   return (
     <div className="shadow-md rounded-full m-2 mx-auto bg-white/20 backdrop-blur-2xl fixed bottom-0 right-0 left-0 w-99/100 border-t-1 border-white/20 flex flex-row items-center justify-between p-1">
-      <NowPlaying />
-      <Player />
-      <VolumeControl />
+      <div className="flex-1 flex justify-start">
+        <NowPlaying />
+      </div>
+      <div className="flex-shrink-0">
+        <Player />
+      </div>
+      <div className="flex-1 flex justify-end">
+        <VolumeControl />
+      </div>
     </div>
   );
 }
