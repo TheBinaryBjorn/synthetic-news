c56a317536dc7bd9c9d24a3e2ca3ef4c0f738ad0
DanielFeldman1 <daniel.feldman303@gmail.com>
2025-09-29 11:41:57 +0300

    style(bottom bar): make bottom bar fully round

--- frontend/src/components/bottom_bar/BottomBar.tsx
+++ frontend/src/components/bottom_bar/BottomBar.tsx
@@ -4,7 +4,7 @@ import VolumeControl from "./VolumeControl";
 
 export default function BottomBar() {
   return (
-    <div className="shadow-md rounded-3xl m-2 mx-auto bg-white/20 backdrop-blur-2xl fixed bottom-0 right-0 left-0 w-99/100 h-20 border-t-1 border-white/20 flex flex-row items-center justify-between px-1">
+    <div className="shadow-md rounded-full m-2 mx-auto bg-white/20 backdrop-blur-2xl fixed bottom-0 right-0 left-0 w-99/100 border-t-1 border-white/20 flex flex-row items-center justify-between p-1">
       <NowPlaying />
       <Player />
       <VolumeControl />
--- frontend/src/components/bottom_bar/NowPlaying.tsx
+++ frontend/src/components/bottom_bar/NowPlaying.tsx
@@ -11,19 +11,19 @@ export default function NowPlaying() {
   ];
   const [mediaNowPlaying, setMediaNowPlaying] = useState(podcasts[0]);
   return (
-    <div className="shadow-md bg-white/1 border-white/10 border-1 backdrop-blur-2xl py-1 rounded-3xl flex flex-row items-center justify-center gap-4 px-1">
+    <div className="shadow-md bg-white/1 border-white/10 border-1 backdrop-blur-2xl py-1 rounded-full flex flex-row items-center justify-center gap-4 px-1">
       {/*Image*/}
       <Image
         src={mediaNowPlaying.thumbnail}
         alt={mediaNowPlaying.title}
         width="64"
         height="64"
-        className="rounded-2xl"
+        className="rounded-full"
       />
       {/*Data*/}
-      <div className="text-white flex flex-col gap-1 mr-2">
+      <div className="text-white flex flex-col gap-1 mr-4">
         <h1 className="text-sm font-semibold">{mediaNowPlaying.title}</h1>
-        <h2 className="text-gray-100 text-xs">{mediaNowPlaying.topic}</h2>
+        <h2 className="text-white/70 text-xs">{mediaNowPlaying.topic}</h2>
       </div>
     </div>
   );
