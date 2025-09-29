3a779a55d7b5f804f18ab951f51c650617372be7
DanielFeldman1 <daniel.feldman303@gmail.com>
2025-09-29 12:31:00 +0300

    style(ui): centered navbar components

--- frontend/src/components/navigation/Navbar.tsx
+++ frontend/src/components/navigation/Navbar.tsx
@@ -6,9 +6,13 @@ import SearchBar from "./SearchBar";
 export default function Navbar() {
   return (
     <nav className="fixed dark:bg-black dark:text-white px-8 top-0 w-screen z-50 bg-white h-15 shadow-md flex flex-row justify-between items-center">
-      <HomeButton />
+      <div className="flex-1 flex justify-start">
+        <HomeButton />
+      </div>
+      <div className="flex-1 flex shrink-0">
       <SearchBar />
-      <div className="flex flex-row items-center gap-4">
+      </div>
+      <div className="flex-1 justify-end flex flex-row items-center gap-4">
         <DarkLightSwitch />
         <ProfileButton />
       </div>
